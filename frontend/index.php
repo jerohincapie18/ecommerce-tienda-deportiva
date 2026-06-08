aaf5606915bc0471e1e07b2f9d81e4cbcbda3aef
jeronimo <jerohincapie@gmail.com>
2026-06-07 22:22:47 -0500

    cambio de ext y validaciones

---
+++ frontend/index.php
@@ -0,0 +1,162 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Draft Tienda de ropa</title>
+    <link rel="stylesheet" href="./assets/css/index-style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <header>
+      <?php include("./components/navbar.php") ?>
+    </header>
+  <section class="hero-section">
+       <img src="./assets/img/mar.jpg" alt="" />
+  </section>
+
+    <section class="store-categories">
+        <button>categoria</button>
+        <button>categoria</button>
+        <button>categoria</button>
+        <button>categoria</button>
+    </section>
+
+    <section class="main-storeSection">
+        <ul class="grid-4" id="ulStoreGrid">
+            <li> 
+                <article class="store-article">
+                    <img src="./assets/img/profilepic.png" alt="">
+                    <div class="article-info">
+                        <div class="article-text">
+                            <div>Nombre</div>
+                            <div>Precio</div>
+                        </div>
+                        <button>favorito</button>
+                    </div>
+                </article>
+            </li>
+            <li> 
+                <article class="store-article">
+                    <img src="./assets/img/profilepic.png" alt="">
+                    <div class="article-info">
+                        <div class="article-text">
+                            <div>Nombre</div>
+                            <div>Precio</div>
+                        </div>
+                        <button>favorito</button>
+                    </div>
+                </article>
+            </li>
+            <li> 
+                <article class="store-article">
+                    <img src="./assets/img/profilepic.png" alt="">
+                    <div class="article-info">
+                        <div class="article-text">
+                            <div>Nombre</div>
+                            <div>Precio</div>
+                        </div>
+                        <button>favorito</button>
+                    </div>
+                </article>
+            </li>
+            <li> 
+                <article class="store-article">
+                    <img src="./assets/img/profilepic.png" alt="">
+                    <div class="article-info">
+                        <div class="article-text">
+                            <div>Nombre</div>
+                            <div>Precio</div>
+                        </div>
+                        <button>favorito</button>
+                    </div>
+                </article>
+            </li>
+            <li> 
+                <article class="store-article">
+                    <img src="./assets/img/profilepic.png" alt="">
+                    <div class="article-info">
+                        <div class="article-text">
+                            <div>Nombre</div>
+                            <div>Precio</div>
+                        </div>
+                        <button>favorito</button>
+                    </div>
+                </article>
+            </li>
+            <li> 
+                <article class="store-article">
+                    <img src="./assets/img/profilepic.png" alt="">
+                    <div class="article-info">
+                        <div class="article-text">
+                            <div>Nombre</div>
+                            <div>Precio</div>
+                        </div>
+                        <button>favorito</button>
+                    </div>
+                </article>
+            </li>
+            <li> 
+                <article class="store-article">
+                    <img src="./assets/img/profilepic.png" alt="">
+                    <div class="article-info">
+                        <div class="article-text">
+                            <div>Nombre</div>
+                            <div>Precio</div>
+                        </div>
+                        <button>favorito</button>
+                    </div>
+                </article>
+            </li>
+            <li> 
+                <article class="store-article">
+                    <img src="./assets/img/profilepic.png" alt="">
+                    <div class="article-info">
+                        <div class="article-text">
+                            <div>Nombre</div>
+                            <div>Precio</div>
+                        </div>
+                        <button>favorito</button>
+                    </div>
+                </article>
+            </li>
+            <li> 
+                <article class="store-article">
+                    <img src="./assets/img/profilepic.png" alt="">
+                    <div class="article-info">
+                        <div class="article-text">
+                            <div>Nombre</div>
+                            <div>Precio</div>
+                        </div>
+                        <button>favorito</button>
+                    </div>
+                </article>
+            </li>
+            <li> 
+                <article class="store-article">
+                    <img src="./assets/img/profilepic.png" alt="">
+                    <div class="article-info">
+                        <div class="article-text">
+                            <div>Nombre</div>
+                            <div>Precio</div>
+                        </div>
+                        <button>favorito</button>
+                    </div>
+                </article>
+            </li>
+        </ul>
+    </section>
+    <div class="show-more-div">
+        <button class="show-more" id="showMore">Mostrar más +</button>
+    </div>
+
+    <footer>
+        <h2>footer</h2>
+    </footer>
+
+    <script src="./assets/js/index-script.js"></script>
+</body>
+</html>
