c47f6bb8acea9b6a17b8f51e079a4413fc3de638
danyi <danyi_troyano82222@elpoli.edu.co>
2026-06-10 23:49:16 -0500

    Mis cambios

--- backend/config/main-config.php
+++ backend/config/main-config.php
@@ -1,10 +1,10 @@
 <?php
 
-$db_data = require("database.php");
-$host = $db_data["DB_HOST"];
-$user = $db_data["DB_USER"];
-$pass = $db_data["DB_PASS"];
-$db_name = $db_data["DB_NAME"];
+//$db_data = require("database.php");
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db_name = "users_db";
 
 $conn = new mysqli($host, $user, $pass, $db_name);
 if ($conn->connect_error)
